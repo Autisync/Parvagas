1a7e5852d955ae7318fa557fc6d8fa9a03da2c5a
parcidio <parcidioandre@gmail.com>
2024-02-09 01:41:11 +0200

    implemented the update application status

--- server/controller/applications.js
+++ server/controller/applications.js
@@ -20,7 +20,7 @@ export const createApplication = async (req, res) => {
       personalStatement,
       curriculumVitae,
       otherDocuments,
-      progress,
+      status,
     } = req.body; // Retreave application fields data from req body
 
     const newApplication = new Application({
@@ -41,7 +41,7 @@ export const createApplication = async (req, res) => {
       personalStatement,
       curriculumVitae,
       otherDocuments,
-      progress,
+      status,
     });// Store the req into a Application object
 
     // Save the application to the database
@@ -50,6 +50,30 @@ export const createApplication = async (req, res) => {
     res.status(201).json({ application: savedApplication, message: 'Application submitted successfully' });
   } catch (error) {
     console.error('Error creating application:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+export const updatestatus = async (req, res) => {
+  try {
+    const { applicationId, status } = req.body; // Retrieve application ID and status from req body
+
+    // Find the application by ID in the database
+    const existingApplication = await Application.findById(applicationId);
+
+    if (!existingApplication) {
+      return res.status(404).json({ error: 'Application not found' });
+    }
+
+    // Update the status field
+    existingApplication.status = status;
+
+    // Save the updated application to the database
+    const updatedApplication = await existingApplication.save();
+
+    res.json({ application: updatedApplication, message: 'Application status updated successfully' });
+  } catch (error) {
+    console.error('Error updating status:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
